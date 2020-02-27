181823f7b28b9ee2514effcec27409b4912b583c
EmreManis <emremn07@gmail.com>
2020-02-27 23:43:04 +0300

    add simple login

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var	app = express();
-var PORT = 80;
+var PORT = 3000;
+var authentication = require('./models/authentication');
 
 	app.set("view engine", "ejs");
+	app.use(authentication.aut);
 
 	app.get("/", function(req,res){
 		res.redirect("index");
@@ -10,6 +12,14 @@ var PORT = 80;
 
 	app.get("/index", function(req, res){
 		res.render("index");
+	});
+
+	//needs work about sessions it doesnt work correctly 
+	app.get("/logout", function(req, res){
+		 
+		res.set('WWW-Authenticate', 'Basic realm="401"');
+		res.status(401).send('Authentication required.');     
+
 	});
 
 	app.listen(PORT, function () {
