39534cc1efe8ea688306c08ddfd44bc0ab3fedc8
EmreManis <emremn07@gmail.com>
2020-06-20 00:47:08 +0300

    correct layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux.js';
+
+const Layout = () => {
+	return (
+		<Aux>
+			<p>Hello World</p>
+			<p>Eren</p>
+		</Aux>
+		);
+};
+
+export default Layout;
