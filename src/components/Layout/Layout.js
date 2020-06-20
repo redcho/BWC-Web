46c770d12612a0ebbb8acdf3562de1bff5d9cfd9
EmreManis <emremn07@gmail.com>
2020-06-20 23:38:36 +0300

    middlescreen setup

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux.js';
 import Toolbar from '../Navigation/Toolbar.js';
+import Stats from '../Stats/Stats';
+
 
 const Layout = () => {
 	return (
 		<Aux>
 			<Toolbar />
+			<Stats />
 		</Aux>
 		);
 };
