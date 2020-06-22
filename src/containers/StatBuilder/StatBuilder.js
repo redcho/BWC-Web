cac3d0fa219fc234497c0790e00f4b8c4a477806
EmreManis <emremn07@gmail.com>
2020-06-22 11:59:39 +0300

    statbuilder setup

---
+++ src/containers/StatBuilder/StatBuilder.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class StatBuilder extends Component {
+	state = {
+
+	};
+	render(){
+		<div>
+			StatBuilder
+		</div>
+	};
+};
+
+export default StatBuilder;
