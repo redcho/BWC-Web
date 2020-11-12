085c25f30c25c1020b44dad00e2533291973b9a8
EmreManis <emremn07@gmail.com>
2020-11-12 11:15:56 +0300

    auth finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import { Redirect, Route, Switch } from "react-router-dom";
@@ -6,26 +6,47 @@ import Stats from "./components/Stats/Stats";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/logout";
 
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-        <Layout/>
-        <Switch>
-            <Route path="/home" component={Stats} />
-            <Route path="/stats" component={Stats} />
-            <Route path="/pvp" component={Stats} />
-            <Route path="/pve" component={Stats} />
-            <Route path="/auctionhouse" component={Stats} />
-            <Route path="/login" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Redirect from="/" to="/login"/>
-        </Switch>
-    </div>
+class App extends Component {
+    render() {
 
-  );
+        let routes = (
+            <Switch>
+                <Route path="/login" component={Auth} />
+                <Route path="/logout" component={Logout} />
+                <Redirect from="/" to="/login"/>
+            </Switch>
+        );
+
+        if(this.props.isAuthenticated) {
+           routes = (
+               <Switch>
+                   <Route path="/home" component={Stats} />
+                   <Route path="/login" component={Auth} />
+                   <Route path="/pvp" component={Stats} />
+                   <Route path="/pve" component={Stats} />
+                   <Route path="/auctionhouse" component={Stats} />
+                   <Route path="/logout" component={Logout} />
+                   <Route path="/stats" component={Stats} />
+                   <Redirect from="/" to="/login"/>
+               </Switch>
+           );
+        }
+        return (
+            <div className="App">
+                <Layout/>
+                {routes}
+            </div>
+
+        );
+    }
 }
 
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
 
-
-export default App;
+export default connect(mapStateToProps)(App);
