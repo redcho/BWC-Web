b6633ef9581dd8c9f154dc497838bf5096c41b08
EmreManis <emremn07@gmail.com>
2020-12-27 22:01:05 +0200

    change layout container to component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,18 +1,36 @@
 import React, {Component} from 'react';
 import Aux from '../Aux';
 import NavigationItem from '../../components/Navigation/NavigationItem.js';
+import DrawerToggle from '../../components/Navigation/DrawerToggle/DrawerToggle';
 
 import { connect } from 'react-redux';
 
 
+
 class Layout extends Component{
+	
+
+	viewport = () => {
+		var win = window,
+			doc = document,
+			docElem = doc.documentElement,
+			body = doc.getElementsByTagName('body')[0],
+			x = win.innerWidth || docElem.clientWidth || body.clientWidth;
+			return x <= 500 ? <DrawerToggle /> : <NavigationItem isAuth ={this.props.isAuthenticated}/>
+
+	}
+
 
 	render() {
 
+
 		return (
 			<Aux>
-				<NavigationItem
-				isAuth ={this.props.isAuthenticated}/>
+					{this.viewport()}
+				{/*{window.addEventListener('resize', this.viewport())}*/}
+
+				{/*<NavigationItem*/}
+				{/*isAuth ={this.props.isAuthenticated}/>*/}
 			</Aux>
 		);
 	};
